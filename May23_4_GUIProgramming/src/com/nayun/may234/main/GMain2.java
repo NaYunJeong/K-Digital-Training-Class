bc5488c0eee4f419ec2c51619fe7379fe41349ee
NaYunJeong <yjna29@gmail.com>
2024-06-01 15:06:32 +0900

    Delete May23_4_GUIProgramming directory

--- May23_4_GUIProgramming/src/com/nayun/may234/main/GMain2.java
+++
@@ -1,9 +0,0 @@
-package com.nayun.may234.main;
-
-import com.nayun.may234.frame.NumberFrame;
-
-public class GMain2 {
-	public static void main(String[] args) {
-		NumberFrame nf = new NumberFrame();
-	}
-}
